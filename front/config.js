8e5d0754141a530650a8c975edb5c61e0d724136
Rocío Vulcano <rocio.vulcano.work@gmail.com>
2025-12-07 17:53:24 -0300

    Fix API URLs for QA and PROD

--- front/config.js
+++ front/config.js
@@ -7,11 +7,11 @@ let API_URL = "";
 
 // Si el dominio contiene "qa" → usar backend QA
 if (window.location.hostname.includes("qa")) {
-  API_URL = "https://changapp-qa.canadacentral-01.azurewebsites.net";
+  API_URL = "https://changapp-qa-f2dkejh0dxeshbh4.canadacentral-01.azurewebsites.net";
 
 // Sino → usar backend PROD
 } else {
-  API_URL = "https://changapp-prod.canadacentral-01.azurewebsites.net";
+  API_URL = "https://changapp-prod-atcjfjgnbkhde0g2.canadacentral-01.azurewebsites.net";
 }
 
 // Exponer variable global
